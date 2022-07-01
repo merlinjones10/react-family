48c5653c0cc0b029c2b4d3590f1df442fd60541e
merlinjones10 <merlinjones10@gmail.com>
2022-07-01 11:27:49 +0100

    setup prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Grandad from './components/Grandad';
-// import Dad from './components/Dad';
-
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Grandad from "./components/Grandad";
+import { Container } from "@mui/material";
 
 function App() {
   // const lastName = 'Smith';
-  const [lastName, setLastName] = useState('Smith');
-  const [fistName, setFirstName] = useState('Smith');
-  
+  const [lastName, setLastName] = useState("Smith");
+  const [fistName, setFirstName] = useState("Smith");
+
   return (
-    <div>
-        <Grandad lastName={lastName} food='chips' setName={setLastName}  />
-  
-
-    </div>
-
+    <>
+      <Container>
+        <Grandad lastName={lastName} food="chips" setName={setLastName} />
+      </Container>
+    </>
   );
 }
 
